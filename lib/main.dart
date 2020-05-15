38d4d7b2ff00d7b5e3240e1883461057cbc80fbd
ujjwal <ujjwalmaity98@gmail.com>
2020-05-15 19:11:53 +0530

    Designing the To-Do List App

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:todoey/screens/tasks_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: TasksScreen(),
+    );
   }
 }
