2ff1d48b4eed7cc346203947d6b97b4c48d926f6
ujjwal <ujjwalmaity98@gmail.com>
2020-05-16 22:17:56 +0530

    Deleting Tasks using the Provider Design Pattern

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -4,12 +4,14 @@ class TaskTile extends StatelessWidget {
   final String taskTitle;
   final bool isChecked;
   final Function checkboxCallback;
+  final Function longPressCallback;
 
-  TaskTile({this.taskTitle, this.isChecked, this.checkboxCallback});
+  TaskTile({this.taskTitle, this.isChecked, this.checkboxCallback, this.longPressCallback});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      onLongPress: longPressCallback,
       title: Text(
         taskTitle,
         style: TextStyle(decoration: isChecked ? TextDecoration.lineThrough : null),
